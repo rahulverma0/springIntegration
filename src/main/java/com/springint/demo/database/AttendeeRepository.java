199d91afa72a58127be1bcb20b13e8b549957189
RV <rahulverma05@yahoo.com>
2020-05-24 06:03:27 -0400

    refactor - core project

--- src/main/java/com/springint/demo/database/AttendeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.springint.demo.database;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AttendeeRepository extends JpaRepository<Attendee, Integer> {
-}
