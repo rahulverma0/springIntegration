9aa8c2774d054f5896be8a4cba0048e469e6fcf1
RV <rahulverma05@yahoo.com>
2020-05-24 06:22:34 -0400

    dsl-based config.

---
+++ project-dsl-based-config/src/main/java/com/springint/demo/database/AttendeeRepository.java
@@ -0,0 +1,6 @@
+package com.springint.demo.database;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AttendeeRepository extends JpaRepository<Attendee, Integer> {
+}
