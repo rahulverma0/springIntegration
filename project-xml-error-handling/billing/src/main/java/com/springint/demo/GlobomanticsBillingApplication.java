aa2231f340aface3be7876c3008a88242e2be612
RV <rahulverma05@yahoo.com>
2020-05-24 13:22:48 -0400

    spring Integration error channel

---
+++ project-xml-error-handling/billing/src/main/java/com/springint/demo/GlobomanticsBillingApplication.java
@@ -0,0 +1,12 @@
+package com.springint.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobomanticsBillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobomanticsBillingApplication.class, args);
+    }
+}
